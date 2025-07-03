7abeb556c0d7dacc6ee7547a07fe57b409f61cee
wtswkz <yw_zhu@qq.com>
2025-07-03 17:55:39 +0800

    WIP@20250703 17:55

--- costas_loop.m
+++ costas_loop.m
@@ -7,7 +7,7 @@ f_d = 4e3;
 
 f_lo_b = 10e6;
 
-N_d = 8000;
+N_d = 4000;
 N = N_d * round(fs / f_d);
 
 d = 2*(randi([0, 1], [1, N_d]) - 0.5);
@@ -22,7 +22,8 @@ bl = 20;
 k1 = 2*kesi*(2*bl/(kesi + 1/(4*kesi))); % tau2carr / tau1carr
 k2 = (2*bl/(kesi + 1/(4*kesi)))^2; % 1 / tau1carr
 
-Tu = 0.001;
+% Tu = 0.001;
+Tu = 1 / fs;
 Dump_N = floor(fs * Tu);
 
 steps = floor(N/Dump_N);
@@ -32,21 +33,27 @@ f_lo = zeros(1, steps+1);
 f_lo(1) = f_lo_b;
 p = 0;
 s = 0;
+x_i = zeros(1, steps);
+x_q = zeros(1, steps);
+x = zeros(1, steps);
 
 for i = 1:steps
-    x = d((i-1)*Dump_N+1:i*Dump_N) .* cos(2*pi*f_in*[(i-1)*Dump_N+1:i*Dump_N]/fs + theta0);
+    x(i) = d((i-1)*Dump_N+1:i*Dump_N) .* sin(2*pi*f_in*[(i-1)*Dump_N+1:i*Dump_N]/fs + theta0);
     % x = cos(2*pi*f_in*[(i-1)*Dump_N:i*Dump_N-1]/fs + theta0);
-    x_i = x .* cos(2*pi*f_lo(i)*[(i-1)*Dump_N:i*Dump_N-1]/fs);
-    x_q = x .* sin(2*pi*f_lo(i)*[(i-1)*Dump_N:i*Dump_N-1]/fs);
+    % x_i = x .* cos(2*pi*f_lo(i)*[(i-1)*Dump_N:i*Dump_N-1]/fs);
+    % x_q = x .* sin(2*pi*f_lo(i)*[(i-1)*Dump_N:i*Dump_N-1]/fs);
+
+    x_i(i) = d((i-1)*Dump_N+1:i*Dump_N) .* cos(2*pi*(f_in - f_lo(i))*[(i-1)*Dump_N+1:i*Dump_N]/fs + theta0);
+    x_q(i) = d((i-1)*Dump_N+1:i*Dump_N) .* sin(2*pi*(f_in - f_lo(i))*[(i-1)*Dump_N+1:i*Dump_N]/fs + theta0);
+    
+    x_i_dump = sum(x_i(i)) / Dump_N;
+    x_q_dump = sum(x_q(i)) / Dump_N;
+
     % x_i = x(1:2:Dump_N);
     % x_q = x(2:2:Dump_N);
     % x_c = (x_i + j*x_q).*(cos(2*pi*f_lo(i)*[(i-1)*Dump_N/2:i*Dump_N/2-1]/fs) - j*sin(2*pi*f_lo(i)*[(i-1)*Dump_N/2:i*Dump_N/2-1]/fs));
-    % x_i = d((i-1)*Dump_N+1:i*Dump_N) .* cos(2*pi*(f_in - f_lo(i))*[(i-1)*Dump_N+1:i*Dump_N]/fs + theta0);
-    % x_q = d((i-1)*Dump_N+1:i*Dump_N) .* sin(2*pi*(f_in - f_lo(i))*[(i-1)*Dump_N+1:i*Dump_N]/fs + theta0);
     % x_i_dump = sum(real(x_c)) / Dump_N * 2;
     % x_q_dump = sum(imag(x_c)) / Dump_N * 2;
-    x_i_dump = sum(x_i) / Dump_N;
-    x_q_dump = sum(x_q) / Dump_N;
 
     % phase_d(i) = sign(x_q_dump)*x_i_dump - sign(x_i_dump)*x_q_dump;
     phase_d(i) = x_i_dump * x_q_dump;
