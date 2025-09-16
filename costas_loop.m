131283ad7b2e6d6b97b5bc9d8be03eda69813593
wtswkz <yw_zhu@qq.com>
2025-09-16 17:57:35 +0800

    change Tu

--- costas_loop.m
+++ costas_loop.m
@@ -7,7 +7,7 @@ f_d = 4e3;
 
 f_lo_b = 1e6;
 
-N_d = 2000;
+N_d = 1000;
 N = N_d * round(fs / f_d);
 
 d = 2*(randi([0, 1], [1, N_d]) - 0.5);
@@ -23,7 +23,7 @@ bl = 20;
 k1 = 2*kesi*(2*bl/(kesi + 1/(4*kesi)));
 k2 = (2*bl/(kesi + 1/(4*kesi)))^2;
 
-Tu = 1e-3;
+Tu = 1e-6;
 % Tu = 1 / fs;
 Dump_N = floor(fs * Tu);
 
@@ -77,26 +77,23 @@ for i = 1:steps
     x_i((i-1)*Dump_N+1:i*Dump_N) = x((i-1)*Dump_N+1:i*Dump_N) .* sin(phi_lo((i-1)*Dump_N+1:i*Dump_N));
     x_q((i-1)*Dump_N+1:i*Dump_N) = x((i-1)*Dump_N+1:i*Dump_N) .* cos(phi_lo((i-1)*Dump_N+1:i*Dump_N));
 
-    if i < length(Num)
-        y_i(i) = Num*[zeros(1, length(Num)-i), x_i(1:i)]' / sum(Num);
-        y_q(i) = Num*[zeros(1, length(Num)-i), x_q(1:i)]' / sum(Num);
-    else
-        y_i(i) = Num*x_i(i-length(Num) + 1:i)' / sum(Num);
-        y_q(i) = Num*x_q(i-length(Num) + 1:i)' / sum(Num);
-    end
+    % if i < length(Num)
+    %     y_i(i) = Num*[zeros(1, length(Num)-i), x_i(1:i)]' / sum(Num);
+    %     y_q(i) = Num*[zeros(1, length(Num)-i), x_q(1:i)]' / sum(Num);
+    % else
+    %     y_i(i) = Num*x_i(i-length(Num) + 1:i)' / sum(Num);
+    %     y_q(i) = Num*x_q(i-length(Num) + 1:i)' / sum(Num);
+    % end
     % 
-    % x_i_dump = sum(y_i(i)) / Dump_N;
-    % x_q_dump = sum(y_q(i)) / Dump_N;
-
-    % x_i(i) = d((i-1)*Dump_N+1:i*Dump_N) .* cos(phi_i((i-1)*Dump_N+1:i*Dump_N) - phi_lo((i-1)*Dump_N+1:i*Dump_N));
-    % x_q(i) = d((i-1)*Dump_N+1:i*Dump_N) .* sin(phi_i((i-1)*Dump_N+1:i*Dump_N) - phi_lo((i-1)*Dump_N+1:i*Dump_N));
+    % x_i_dump(i) = sum(y_i(i)) / Dump_N;
+    % x_q_dump(i) = sum(y_q(i)) / Dump_N;
 
     x_i_dump(i) = sum(x_i((i-1)*Dump_N+1:i*Dump_N)) / Dump_N;
-    x_q_dump(i)  = sum(x_q((i-1)*Dump_N+1:i*Dump_N)) / Dump_N;
+    x_q_dump(i) = sum(x_q((i-1)*Dump_N+1:i*Dump_N)) / Dump_N;
 
     % phase_d(i) = 1/sqrt(2) * (sign(x_i_dump)*x_q_dump - sign(x_q_dump)*x_i_dump);
     % phase_d(i) = -sign(x_i_dump)*x_q_dump;
-    phase_d(i) = x_i_dump(i)  * x_q_dump(i)  * 4;
+    phase_d(i) = x_i_dump(i) * x_q_dump(i) * 4;
     % phase_d(i) = x_i_dump * x_q_dump;
     % if i < length(Num1)
     %     phase_d_lpf(i) = Num1*[zeros(1, length(Num1)-i), phase_d(1:i)]' / sum(Num1);
